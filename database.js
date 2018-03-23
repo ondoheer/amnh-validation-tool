3563d14d17cebf878424701b005d3eb72afda5e9
Sean Rankine <sean.rankine@me.com>
2018-03-23 16:42:25 -0400

    Add db init and add record function

--- database.js
+++ database.js
@@ -1,27 +1,49 @@
-let db
-
 function initDB () {
-
     if (!window.indexedDB) {
-        window.alert("Your browser doesn't support a stable version of IndexedDB. Please use a compatible browser.")
+        window.alert('Your browser doesnt support a stable version of IndexedDB. Please use a compatible browser.')
     }
 
-    var request = window.indexedDB.open("MarineSpecimensData", 1)
+    var request = window.indexedDB.open('MarineDatabase', 1)
 
     request.onerror = (event) => {
-        console.error("You need to allow IndexedDB to use this application.")
+        console.error('You need to allow IndexedDB to use this application.')
     };
 
     request.onsuccess = (event) => {
-        console.log("Using IndexedDB!")
+        console.log('Using IndexedDB!')
         db = event.target.result
         db.onerror = function(event) {
-            console.error("Database error: " + event.target.errorCode)
+            console.error('Database error: ' + event.target.errorCode)
         };
     };
 
     request.onupgradeneeded = (event) => {
-        console.log("Upgrading DB")
+        console.log('Creating/Upgrading DB')
+        db = event.target.result
+        db.createObjectStore('records',  {keyPath: 'id', autoIncrement: true})
     }
 }
 
+function addRecord (record) {
+    var transaction = db.transaction(['records'], 'readwrite')
+    transaction.oncomplete = (event) => {
+        console.log('Transaction all done!')
+    }
+      
+    transaction.onerror = (event) => {
+        console.error('Bad transaction')
+    }
+    
+    var objectStore = transaction.objectStore('records')
+    var request = objectStore.add(record)
+
+    request.onsuccess = (event) => {
+        console.log('Record added!')
+    }
+
+    request.onerror = (event) => {
+        console.error('Whomp whomp no record added.')
+    }
+}
+
+initDB()
