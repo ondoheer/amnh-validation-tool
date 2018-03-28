dd8ed10d22abd295e2b26f6ba27c8c2a51ebb647
Eric Perrotta <emperrotta@gmail.com>
2018-03-28 13:05:30 -0400

    create listener for add person button
    move from index.js to add_person.js

---
+++ static/js/add_person.js
@@ -0,0 +1,26 @@
+let determinedByList = document.getElementById('determined-by-list');
+let determinedPersonBttn = document.getElementById('determined-person-bttn');
+let determinedByForm = document.querySelectorAll('#determined-by-form input');
+
+let determinedByPeople = [];
+
+function addDeterminedByPerson() {
+    let person = {};
+
+    determinedByForm.forEach((element) => {
+        person[element.placeholder] = element.value;
+        element.value = '';
+    })
+
+    determinedByPeople.push(person);
+    let listItem = document.createElement('li');
+    let span = document.createElement('span');
+    let i = document.createElement('i');
+    i.classList.add("fas", "fa-user");
+    span.appendChild(i);
+    listItem.appendChild(span);
+    determinedByList.appendChild(listItem);
+    listItem.innerHTML += person['Verbatim Name'];
+}
+
+determinedPersonBttn.addEventListener('click', addDeterminedByPerson);
